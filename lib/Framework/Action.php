54b032852d8a92e2424c7bb2c6ca9cf563edbf21
Sushil Chaturvedi <sushil.c@outlook.com>
2013-08-14 10:07:15 +0530

    adding get/post accessors

--- lib/Framework/Action.php
+++ lib/Framework/Action.php
@@ -13,4 +13,28 @@ class Action {
         return $this->template;
     }
     
+    
+    public function getAllGetParams() {
+        return $_GET;
+    }
+    
+    public function getAllPostParams() {
+        return $_POST;
+    }
+
+    public function getAllRequestParams() {
+        return $_REQUEST;
+    }
+
+    public function isPostRequest() {
+        return count($_POST);
+    }
+    
+    public function getPostVar($key) {
+        return $_POST[$key];
+    }
+    
+    public function getGetVar($key) {
+        return $_GET[$key];
+    }
 }
